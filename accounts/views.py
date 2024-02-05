418750650b03e8476cd84c5bdb27884f2aec2194
hmdnz <hammadmcferri@gmail.com>
2024-02-05 10:44:04 +0100

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'dashboard.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def customers(request):
+    
+    return render(request, 'customers.html')
+
